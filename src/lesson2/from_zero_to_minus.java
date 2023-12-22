4fd667e3fd9495aab601e38c47bb59ca6d6af542
Ilya8633 <ilprez28@gmail.com>
2023-12-22 22:12:19 +0300

    lesson #2 else - last tasks from cycle

---
+++ src/lesson2/from_zero_to_minus.java
@@ -0,0 +1,9 @@
+package lesson2;
+
+public class from_zero_to_minus {
+    public static void main(String[] args) {
+        for (int i = 0; i >= -50; i-=5) {
+            System.out.print(i+" ");
+        }
+    }
+}
