de62f695e0208a3d8c86aa364ab6974972e2b23d
Ilya8633 <ilprez28@gmail.com>
2023-12-22 18:04:33 +0300

    lesson #2 else - warm-ok-cold

--- src/lesson2/TemperatureOutside.java
+++ src/lesson2/TemperatureOutside.java
@@ -1,4 +1,16 @@
 package lesson2;
 
 public class TemperatureOutside {
+    public static void main(String[] args) {
+        int temperature = -56;
+
+        if (temperature>-5){
+            System.out.println("warm");
+        }if (temperature<=-5 && temperature>=-20){
+            System.out.println("normal temperature");
+        }if ( temperature<=-20){
+            System.out.println("cold");
+
+        }
+    }
 }
