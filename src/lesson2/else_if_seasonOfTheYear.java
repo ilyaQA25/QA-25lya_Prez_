3265be0d3bc9722cf798354d215708a3d9daad95
Ilya8633 <ilprez28@gmail.com>
2023-12-22 17:28:48 +0300

    lesson #2 else - if season of the year

---
+++ src/lesson2/else_if_seasonOfTheYear.java
@@ -0,0 +1,46 @@
+package lesson2;
+
+public class else_if_seasonOfTheYear {
+    public static void main(String[] args) {
+        int numberofthemonth = 1;
+
+        if (numberofthemonth == 1) {
+            System.out.println("winter");
+        }
+        if (numberofthemonth == 2) {
+            System.out.println("winter");
+        }
+        if (numberofthemonth == 3) {
+            System.out.println("spring");
+        }
+        if (numberofthemonth == 4) {
+            System.out.println("spring");
+        }
+        if (numberofthemonth == 5) {
+            System.out.println("spring");
+        }
+        if (numberofthemonth == 6) {
+            System.out.println("summer");
+        }
+        if (numberofthemonth == 7) {
+            System.out.println("summer");
+        }
+        if (numberofthemonth == 8) {
+            System.out.println("summer");
+        }
+        if (numberofthemonth == 9) {
+            System.out.println("autumn");
+        }
+        if (numberofthemonth == 10) {
+            System.out.println("autumn");
+        }
+        if (numberofthemonth == 11) {
+            System.out.println("autumn");
+        }
+        if (numberofthemonth == 12) {
+            System.out.println("winter");
+        } else {
+            System.out.println("wrong number");
+        }
+    }
+}
