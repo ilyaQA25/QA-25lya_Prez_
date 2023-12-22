67de15487121bfaad2b73afdfa8044d28b3f2a27
Ilya8633 <ilprez28@gmail.com>
2023-12-22 21:33:32 +0300

    lesson #2 else - cycle_first two tasks

---
+++ src/lesson2/five_to_one.java
@@ -0,0 +1,9 @@
+package lesson2;
+
+public class five_to_one {
+    public static void main(String[] args) {
+        for (int i = 5; i >=1 ; i--) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/lesson2/odd_one_to_99.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class odd_one_to_99 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 99; i++) {
+            if(i%2!=0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
