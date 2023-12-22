8de48603ccf28803325562b0c93f1b66b18e0e55
Ilya8633 <ilprez28@gmail.com>
2023-12-22 17:42:30 +0300

    lesson #2 else - even-odd number

--- src/lesson2/even_Odd.java
+++ src/lesson2/even_Odd.java
@@ -1,4 +1,16 @@
-package lesson2;
 
+package lesson2;
+import java.util.Scanner;
 public class even_Odd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your number");
+        int userNumber = scanner.nextInt();
+
+        if (userNumber % 2==0 && userNumber>0){
+            System.out.println("this number is even");
+        }else{
+            System.out.println("it is odd number or negative number");
+        }
+    }
 }
