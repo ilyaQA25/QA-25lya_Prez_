9666cd1f935a08b230c1e419e863d6983cfddd57
Ilya8633 <ilprez28@gmail.com>
2023-12-26 23:06:50 +0300

    HW #3 task #0

---
+++ src/lesson3_HW/Task_zero.java
@@ -0,0 +1,25 @@
+package lesson3_HW;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task_zero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your number: ");
+        int userNumber = scanner.nextInt();
+
+        int[] mas = {25, 47, 34, 18, 96, 33, 28, 55, 87, 13};
+
+        for (int i = 0; i < mas.length; i++) {
+            if (mas[i] == userNumber) {
+                int index = i;
+                System.out.println("your number is included in array");
+                System.out.println("index is " + index);
+            }
+            }
+
+        }
+
+    }
+
