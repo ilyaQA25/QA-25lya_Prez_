3cf0f06c8fc68f3bdfd56ef9319cc7e7fef5724b
Ilya8633 <ilprez28@gmail.com>
2024-01-22 16:31:20 +0300

    HW #7 solve problems

--- src/lesson7/hw/facility/Accountant.java
+++ src/lesson7/hw/facility/Accountant.java
@@ -1,14 +1,10 @@
 package lesson7.hw.facility;
 
 class Accountant implements PrintingJobTitle{
-    String jobTitle = "Accountant";
 
-    public Accountant(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
 
     @Override
     public String printJobTitle() {
-        return jobTitle;
+        return "Accountant";
     }
 }
