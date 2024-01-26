ecd2d52d51496351d6521944a9848c5f7d07f003
Ilya8633 <ilprez28@gmail.com>
2024-01-26 23:52:00 +0300

    hw10 task 0.2 done

--- src/lesson10/hw/Strings.java
+++ src/lesson10/hw/Strings.java
@@ -1,6 +1,12 @@
 package lesson10.hw;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
 public class Strings {
+
+
 
     public String task1(String value) {
         String firstPart = null;
@@ -12,6 +18,49 @@ public class Strings {
             secondPart = array[2];
         }
         return firstPart + secondPart;
+    }
+    public String task2(String value) {
+        String firstPart = null;
+        String secondPart = null;
+        String thirdPart = null;
+        String fourthPart = null;
+        String fivePart = null;
+        var array = value.split("-");
+
+        for (int i = 0; i < array.length; i++) {
+            firstPart = array[0];
+            secondPart = array[1];
+            thirdPart = array[2];
+            fourthPart = array[3];
+            fivePart = array[4];
+        }
+
+        char b = '*';
+        var newval1 = secondPart.toCharArray();
+        for (int i = 0; i < newval1.length; i++) {
+            newval1[i]=b;
+            System.out.print(newval1[i]);
+
+        }
+
+        /*String[] stringArray = { "a", "b", "c", "d", "e" };
+        ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(stringArray));
+        System.out.println(arrayList);
+// [a, b, c, d, e]*/
+
+
+        System.out.println();
+        char a = '*';
+        var newval2 = fourthPart.toCharArray();
+        for (int i = 0; i < newval2.length; i++) {
+            newval2[i]= a;
+            System.out.print(newval2[i]);
+        }
+
+        System.out.println();
+        return firstPart+"-" +Arrays.toString(newval1).replace("[", "").replace("]", "").replace(",", "").replace(" ","")
+                +"-"+thirdPart+"-"+Arrays.toString(newval2).replace("[", "").replace("]", "").replace(",", "").replace(" ","")+"-"+fivePart;
+
     }
 
 //    public String task2withStars(String value) {
@@ -46,19 +95,7 @@ public class Strings {
 //
 //    }
 
-    public String task2(String value) {
-        var array = value.toCharArray();
-        char a = '*';
-        int b = 0;
-        for (int i = 0; i < array.length; i++) {
-            ;
-            if (b == 0) {
-                array[0]= a;
-                System.out.println(array[i]);
-            }
-        }
-        return value;
-    }
+
 
     /*    char a = ' ';
         char b = ' ';
@@ -86,17 +123,18 @@ public class Strings {
         }
 
         char a = ' ';
-        char b = ' ';
-
+        char b= ' ';
         var newval = thirdPart.toCharArray();
 
         for (int i = 0; i < newval.length; i++) {
-            a = newval[1];
+            a= newval[1];
             b = newval[3];
+
         }
 
 
-        return firstPart + "/" + secondPart + "/" + a + "/" + b;
+        return firstPart.toLowerCase() + "/" + secondPart.toLowerCase() + "/" + a + "/" + b;
+        //как привести последние элементы к lowerCase хз ваще
     }
 
 }
