7af56a71bc5d7ca08a844f41ae76fb5edcd67e9a
Ilya8633 <ilprez28@gmail.com>
2024-01-26 22:26:15 +0300

    hw10 dont know how to solve task 2

---
+++ src/lesson10/hw/Main.java
@@ -0,0 +1,14 @@
+package lesson10.hw;
+
+public class Main {
+    public static void main(String[] args) {
+        Strings strings = new Strings();
+
+        String value = "1234-abc-5678-edc-1q1q";
+
+       System.out.println(strings.task1(value));  //1
+        System.out.println(strings.task2(value));
+       // System.out.println(strings.task3(value));
+
+    }
+}
