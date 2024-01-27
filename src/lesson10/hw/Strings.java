bd07758dc96e76343799bdf81f1f4d5b68c5bf7d
Ilya8633 <ilprez28@gmail.com>
2024-01-27 20:52:17 +0300

    hw10 done

--- src/lesson10/hw/Strings.java
+++ src/lesson10/hw/Strings.java
@@ -7,7 +7,6 @@ import java.util.Collection;
 public class Strings {
 
 
-
     public String task1(String value) {
         String firstPart = null;
         String secondPart = null;
@@ -19,6 +18,7 @@ public class Strings {
         }
         return firstPart + secondPart;
     }
+
     public String task2(String value) {
         String firstPart = null;
         String secondPart = null;
@@ -38,78 +38,25 @@ public class Strings {
         char b = '*';
         var newval1 = secondPart.toCharArray();
         for (int i = 0; i < newval1.length; i++) {
-            newval1[i]=b;
-            System.out.print(newval1[i]);
-
+            newval1[i] = b;
         }
-
-        /*String[] stringArray = { "a", "b", "c", "d", "e" };
-        ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(stringArray));
-        System.out.println(arrayList);
-// [a, b, c, d, e]*/
 
 
         System.out.println();
         char a = '*';
         var newval2 = fourthPart.toCharArray();
         for (int i = 0; i < newval2.length; i++) {
-            newval2[i]= a;
-            System.out.print(newval2[i]);
+            newval2[i] = a;
+
         }
 
-        System.out.println();
-        return firstPart+"-" +Arrays.toString(newval1).replace("[", "").replace("]", "").replace(",", "").replace(" ","")
-                +"-"+thirdPart+"-"+Arrays.toString(newval2).replace("[", "").replace("]", "").replace(",", "").replace(" ","")+"-"+fivePart;
+        return firstPart + "-" +
+                Arrays.toString(newval1).replace("[", "").replace("]", "").replace(",", "").replace(" ", "")
+                + "-" + thirdPart + "-" + Arrays.toString(newval2).replace("[", "").replace("]", "").replace(",", "").replace(" ", "") + "-" + fivePart;
 
     }
 
-//    public String task2withStars(String value) {
-//        String firstPart = null;
-//        String secondPart = null;
-//        var array = value.split("-");
-//
-//        for (int i = 0; i < array.length; i++) {
-//            firstPart = array[1];
-//            secondPart = array[3];
-//        }
-//
-//        char a = '*';
-//
-//        var neval1 = firstPart.toCharArray();
-//        for (int i = 0; i < neval1.length; i++) {
-//            neval1[i] = a;
-//            System.out.print(neval1[i]);
-//
-//        }
-//
-//        System.out.print("/");
-//        var neval2 = secondPart.toCharArray();
-//        for (int i = 0; i < neval2.length; i++) {
-//            neval2[i] = a;
-//            System.out.print(neval2[i]);
-//
-//        }
-//
-//        return value;
-//
-//
-//    }
 
-
-
-    /*    char a = ' ';
-        char b = ' ';
-
-        var newval = thirdPart.toCharArray();
-
-            for (int i = 0; i < newval.length; i++) {
-            a = newval[1];
-            b = newval[3];
-        }
-
-
-            return firstPart + "/" + secondPart + "/" + a + "/" + b;
-    }*/
     public String task3(String value) {
         String firstPart = null;
         String secondPart = null;
@@ -123,18 +70,51 @@ public class Strings {
         }
 
         char a = ' ';
-        char b= ' ';
+        char b = ' ';
         var newval = thirdPart.toCharArray();
-
+        System.out.println(Arrays.toString(newval));
         for (int i = 0; i < newval.length; i++) {
-            a= newval[1];
+            a = newval[1];
             b = newval[3];
 
         }
-
+        // cпросить, почему записи a = newval[1] и запись newval[1]=а не одно и тоже!
 
         return firstPart.toLowerCase() + "/" + secondPart.toLowerCase() + "/" + a + "/" + b;
         //как привести последние элементы к lowerCase хз ваще
     }
+
+    public String task4(String value) {
+        StringBuilder value1 = new StringBuilder(value);
+        var array = value1.delete(0, 5).delete(3, 8).delete(8, 9).delete(9, 10);
+        return array.toString().toUpperCase();
+    }
+
+
+    public String task5(String value) {
+        if (value.contains("abc") || value.contains("ABC")) {
+            System.out.println("abc is contained");
+        }
+
+        return value;
+
+    }
+
+    public String task6(String value) {
+        if (value.startsWith("555")) {
+            System.out.println("string start with 555");
+        }
+
+        return value;
+    }
+
+    public String task7(String value) {
+        if (value.endsWith("1a2b")) {
+            System.out.println("string ended with 1a2b");
+        }
+
+        return value;
+    }
+
 
 }
