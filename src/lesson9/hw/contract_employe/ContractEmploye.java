ba97762b09dd60a15518170d74954cf71d30d169
Ilya8633 <ilprez28@gmail.com>
2024-02-02 12:30:45 +0300

    hw9 pack

--- src/lesson9/hw/contract_employe/ContractEmploye.java
+++ src/lesson9/hw/contract_employe/ContractEmploye.java
@@ -1,30 +1,30 @@
-package lesson9.hw.contractEmploye;
+package lesson9.hw.contract_employe;
 
 import lesson9.hw.documents.Documents;
 
 import java.util.Date;
 
 public class ContractEmploye extends Documents {
-    Integer docNumber;
     Date docDate;
     Date contractDeadline;
 
     String employeName;
 
-    public ContractEmploye(Integer docNumber, Date docDate, Date contractDeadline, String employeName) {
-        this.docNumber = docNumber;
+    public ContractEmploye(String docNumber, Date docDate, Date contractDeadline, String employeName) {
+        super(docNumber);
         this.docDate = docDate;
         this.contractDeadline = contractDeadline;
         this.employeName = employeName;
     }
 
-    public ContractEmploye() {
+    public ContractEmploye(String docNumber) {
+        super(docNumber);
     }
 
     @Override
     public String toString() {
         return "ContractEmploye{" +
-                "docNumber=" + docNumber +
+                "docNumber=" + this.getDocNumber() +
                 ", docDate=" + docDate +
                 ", contractDeadline=" + contractDeadline +
                 ", employeName='" + employeName + '\'' +
